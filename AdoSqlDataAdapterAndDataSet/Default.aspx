5e3e7e46c80dc3c4e404a77b8a7c7fdfe5e30b97
Anuraag Godse <anurag.godse@gmail.com>
2021-03-30 00:23:48 +0530

    Add project files.

---
+++ AdoSqlDataAdapterAndDataSet/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AdoSqlDataAdapterAndDataSet._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label2" runat="server" Text="Enter Name:"></asp:Label>
+    <asp:TextBox ID="TxtName" runat="server"></asp:TextBox>
+    <asp:Button ID="btnShowTxtBoxData" runat="server" Text="Show Data" OnClick="btnShowTxtBoxData_Click" />
+    <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+        <AlternatingRowStyle BackColor="White" />
+        <EditRowStyle BackColor="#7C6F57" />
+        <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#E3EAEB" />
+        <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#F8FAFA" />
+        <SortedAscendingHeaderStyle BackColor="#246B61" />
+        <SortedDescendingCellStyle BackColor="#D4DFE1" />
+        <SortedDescendingHeaderStyle BackColor="#15524A" />
+    </asp:GridView>
+    
+    <asp:Button ID="Button1" runat="server" Text="Load Data" Width="85px" OnClick="Button1_Click" />&nbsp;
+    <asp:Button ID="Button2" runat="server" Text="Clear Cache" OnClick="Button2_Click" />
+    <br />
+
+    <asp:Label ID="Label1" runat="server"></asp:Label>
+
+
+    <asp:GridView ID="GridView2" runat="server" BackColor="White" BorderColor="#E7E7FF" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Horizontal">
+        <AlternatingRowStyle BackColor="#F7F7F7" />
+        <FooterStyle BackColor="#B5C7DE" ForeColor="#4A3C8C" />
+        <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+        <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+        <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+        <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+        <SortedAscendingCellStyle BackColor="#F4F4FD" />
+        <SortedAscendingHeaderStyle BackColor="#5A4C9D" />
+        <SortedDescendingCellStyle BackColor="#D8D8F0" />
+        <SortedDescendingHeaderStyle BackColor="#3E3277" />
+    </asp:GridView>
+
+
+</asp:Content>
---
+++ AdoSqlDataAdapterAndDataSet/Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AdoSqlDataAdapterAndDataSet
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using(SqlConnection con = new SqlConnection(CS))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("GetDataSetData", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds.Tables[0];
+                GridView1.DataBind();
+
+                GridView2.DataSource = ds.Tables[1];
+                GridView2.DataBind();
+            }
+        }
+        
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            if (Cache["Data"] == null)
+            {
+                using (SqlConnection con = new SqlConnection(CS))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter("select * from Student", con);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    GridView1.DataSource = ds;                    
+                    GridView1.DataBind();
+                    Cache["Data"] = ds;
+                    Label1.Text = "Data loaded from database";
+                }
+            }
+            else
+            {
+                GridView1.DataSource = Cache["Data"];
+                GridView1.DataBind();
+                Label1.Text = "Data loaded from cache";
+            }
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (Cache["Data"] != null)
+            {
+                Cache.Remove("Data");
+            }
+        }
+
+        protected void btnShowTxtBoxData_Click(object sender, EventArgs e)
+        {
+            GridView1.Visible = false;
+            string inputName = TxtName.Text;
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using(SqlConnection con = new SqlConnection(CS))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("GetDataSetDataWithInputParam", con);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.AddWithValue("@Name", inputName);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                GridView1.Visible = true;
+            }
+        }
+    }
+}
